437aaedbff797b833e23d603768d10317f62cddf
flowitoo <69961128+flowitoo@users.noreply.github.com>
2021-01-28 23:48:38 +0100

    Add files via upload

--- htb-invite-generator.py
+++ htb-invite-generator.py
@@ -1,4 +1,1 @@
-from subprocess import PIPE, run;import json;import base64
-def request(command):
-    codegen = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True);return codegen.stdout
-loadit=json.loads(request("curl -XPOST https://www.hackthebox.eu/api/invite/generate -s"));code=base64.b64decode(loadit['data']['code']);print(f"Your HackTheBox invite is {str(code, 'utf-8')}")
+from subprocess import PIPE, run;import json;import base64;c="curl -XPOST https://www.hackthebox.eu/api/invite/generate -s";g=run(c, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True);l=json.loads(g.stdout);cd=base64.b64decode(l['data']['code']);print(f"Your HackTheBox invite is {str(cd, 'utf-8')}")
