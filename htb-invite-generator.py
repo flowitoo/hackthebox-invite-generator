08a7f1921331b9c2fbb8e8ae77a9df47e859aaef
flowitoo <69961128+flowitoo@users.noreply.github.com>
2021-01-28 23:35:36 +0100

    Update htb-invite-generator.py

--- htb-invite-generator.py
+++ htb-invite-generator.py
@@ -1,5 +1,4 @@
 from subprocess import PIPE, run;import json;import base64
 def request(command):
-    codegen = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    return codegen.stdout
+    codegen = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True);return codegen.stdout
 loadit=json.loads(request("curl -XPOST https://www.hackthebox.eu/api/invite/generate -s"));code=base64.b64decode(loadit['data']['code']);print(f"Your HackTheBox invite is {str(code, 'utf-8')}")
